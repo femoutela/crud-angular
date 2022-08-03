3c5f66d5c752dac54ab689fe2e18cf460b053d76
femoutela <femoutela@msn.com>
2022-08-03 16:48:17 -0300

    Criação do projeto

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,29 @@
+import { CoursesService } from './../services/courses.service';
+import { Course } from './../model/course';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+   courses: Observable <Course[]>;
+   displayedColumns = ['name','category'];
+
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+
+   this.courses = this.coursesService.list();
+
+
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
